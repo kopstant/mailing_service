7364ed763408dda503cab3195ef1c3ae66d9553d
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2025-03-05 08:38:40 -0400

    Загружены сторонние библиотеки для корректной работы приложения. Реализована модель users для регистрации, входа, выхода из сервиса. Поля: email, avatar, phone_number, country в соответсвующих типах.

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, max_length=254, verbose_name="email")
+    avatar = models.ImageField(upload_to="users/avatars/", verbose_name="avatar", **NULLABLE)
+    phone_number = models.CharField(max_length=11, unique=True, verbose_name="phone number", **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name="country", **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
